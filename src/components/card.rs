2baa8fe965c5070d330afbb05ee54b4a286a1706
Spheniscine <spheniscine@gmail.com>
2026-09-22 15:54:07 +0800

    draft for tweaked card design

---
+++ src/components/card.rs
@@ -0,0 +1,144 @@
+use dioxus::prelude::*;
+use glam::Vec2;
+
+use crate::{components::rem, game::{Card, ColorMode, Skin, Suit}};
+
+pub trait SkinTrait<C>: PartialEq + Clone {
+    fn get_color(&self, card: &C, mode: ColorMode) -> String;
+    fn render_rank(&self, card: &C) -> Element;
+    fn render_suit(&self, card: &C) -> Element;
+    fn render_suit_text(&self, card: &C) -> Element;
+}
+
+pub const BASE_CARD_WIDTH: f32 = 8.;
+pub const BASE_CARD_HEIGHT: f32 = 12.;
+pub const CARD_HEIGHT_RATIO: f32 = BASE_CARD_HEIGHT / BASE_CARD_WIDTH;
+pub const CARD_BORDER_RADIUS_RATIO: f32 = 1. / BASE_CARD_WIDTH;
+
+#[component]
+pub fn BaseCardComponent<C: PartialEq + Clone + 'static, S: SkinTrait<C> + 'static>(
+    position: Vec2,
+    width: f32,
+    card: C,
+    skin: S,
+
+    // number_hint: Option<usize>,
+    #[props(default = ColorMode::Light)]
+    color_mode: ColorMode,
+
+    #[props(default)]
+    onclick: EventHandler<MouseEvent>,
+    #[props(default)]
+    ondoubleclick: EventHandler<MouseEvent>,
+) -> Element {
+    let pt = width / BASE_CARD_WIDTH;
+    let pt = |x: f32| {
+        rem(x * pt)
+    };
+
+    rsx! {
+        div {
+            style: "place-items: center;",
+            position: "absolute",
+            top: rem(position.y),
+            left: rem(position.x),
+            background_color: color_mode.choose("#fff", "#101"),
+            width: pt(BASE_CARD_WIDTH - 1.),
+            height: pt(BASE_CARD_HEIGHT - 1.),
+            border: "{pt(0.25)} solid",
+            border_color: color_mode.choose("#000", "#868"),
+            border_radius: rem(width * CARD_BORDER_RADIUS_RATIO),
+            display: "grid",
+            grid_template_columns: "50% 50%",
+            grid_template_rows: "50% 50%",
+            font_size: pt(3.5),
+            text_align: "center",
+            padding: pt(0.25),
+            color: skin.get_color(&card, !color_mode),
+
+            onclick, ondoubleclick,
+
+            div { display: "flex", align_items: "center", pointer_events: "none", {skin.render_rank(&card)}},
+            div { display: "flex", align_items: "center", pointer_events: "none", {skin.render_suit(&card)}},
+            div { display: "flex", align_items: "center", pointer_events: "none", {skin.render_suit(&card)}},
+            div { display: "flex", align_items: "center", pointer_events: "none", {skin.render_rank(&card)}},
+        }
+    }
+}
+
+pub const CARD_FRAME_DEFAULT_COLOR: &str = "#aaa";
+
+#[component]
+pub fn CardFrame(
+    position: Vec2,
+    width: f32,
+    hint: Option<Element>,
+    #[props(default = CARD_FRAME_DEFAULT_COLOR.to_string())] 
+    color: String,
+    onclick: EventHandler<MouseEvent>,
+    // oncontextmenu: EventHandler<MouseEvent>,
+    #[props(default)] dashed: bool,
+
+    number_hint: Option<i32>,
+) -> Element {
+    let color = color.as_str();
+    let pt = width / BASE_CARD_WIDTH;
+    let pt = |x: f32| {
+        rem(x * pt)
+    };
+    let border_style = if dashed {"dashed"} else {"solid"};
+    rsx! {
+        div {
+            display: "flex",
+            align_items: "center",
+            justify_content: "center",
+            position: "absolute",
+            top: rem(position.y),
+            left: rem(position.x),
+            margin: pt(0.25), // frame must be slightly smaller than card to prevent peeking out in some platforms
+            width: pt(BASE_CARD_WIDTH - 2.),
+            height: pt(BASE_CARD_HEIGHT - 2.),
+            border: "{pt(0.5)} {border_style} {color}",
+            text_align: "center",
+            color,
+            border_radius: rem(width * CARD_BORDER_RADIUS_RATIO),
+            font_size: pt(3.5),
+            padding: pt(0.25),
+            onclick, 
+            //oncontextmenu,
+
+            if let Some(hint) = hint {
+                div {
+                    {hint},
+                }
+            }
+        }
+    }
+}
+
+#[component]
+pub fn CardComponent(
+    position: Vec2,
+    width: f32,
+    card: Card,
+    skin: Skin,
+
+    #[props(default)]
+    onclick: EventHandler<MouseEvent>,
+    #[props(default)]
+    ondoubleclick: EventHandler<MouseEvent>,
+    #[props(default)]
+    oncontextmenu: EventHandler<MouseEvent>,
+) -> Element {
+    let color_mode = if card.suit == Suit::Trump {ColorMode::Dark} else {ColorMode::Light};
+    rsx! {
+        BaseCardComponent { 
+            position, width, 
+            card, 
+            skin,
+            color_mode,
+            onclick,
+            ondoubleclick,
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,16 @@
 use dioxus::prelude::*;
+use glam::Vec2;
+
+use crate::{components::CardComponent, game::{Card, Skin}};
 
 mod game;
+mod components;
 
 const FAVICON: Asset = asset!("/assets/favicon.ico");
+
+// altered version of KaTeX_Main to include filled "red" suits
+const KATEX_SUITS: Asset = asset!("/assets/KaTeX_Suits.woff2");
+
 const MAIN_CSS: Asset = asset!("/assets/main.css");
 const HEADER_SVG: Asset = asset!("/assets/header.svg");
 
@@ -13,7 +21,37 @@ fn main() {
 #[component]
 fn App() -> Element {
     rsx! {
+        document::Link {
+            rel: "stylesheet",
+            href: "https://cdn.jsdelivr.net/npm/katex@0.16.21/dist/katex.min.css",
+            integrity: "sha384-zh0CIslj+VczCZtlzBcjt5ppRcsAmDnRem7ESsYwWwg3m/OaJ2l4x7YBZl9Kxxib",
+            crossorigin: "anonymous"
+        }
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.googleapis.com",
+        }
+        document::Link {
+            rel: "preconnect",
+            href: "https://fonts.gstatic.com",
+            crossorigin: "anonymous",
+        }
+        document::Link {
+            href: "https://fonts.googleapis.com/css2?family=Noto+Emoji:wght@300..700&family=Noto+Sans+Math&family=Noto+Sans+Symbols+2&family=Noto+Sans+Symbols:wght@100..900&family=Noto+Sans:ital,wght@0,100..900;1,100..900&display=swap",
+            rel: "stylesheet",
+        }
         document::Link { rel: "icon", href: FAVICON }
+        document::Style {
+            r#"
+            @font-face {{
+                font-family: KaTeX_Suits;
+                font-style: normal;
+                font-weight: 700;
+                src: url({KATEX_SUITS}) format("woff2");
+            }} 
+            "#,
+        }
+
         document::Link { rel: "stylesheet", href: MAIN_CSS }
         Hero {}
 
@@ -22,17 +60,18 @@ fn App() -> Element {
 
 #[component]
 pub fn Hero() -> Element {
+    let card = Card { rank: 13, suit: game::Suit::Spades };
+    let skin = Skin { suits: game::SuitSkin::Animals, colors: game::ColorSkin::FourColor };
+
     rsx! {
         div {
             id: "hero",
-            img { src: HEADER_SVG, id: "header" }
-            div { id: "links",
-                a { href: "https://dioxuslabs.com/learn/0.6/", "📚 Learn Dioxus" }
-                a { href: "https://dioxuslabs.com/awesome", "🚀 Awesome Dioxus" }
-                a { href: "https://github.com/dioxus-community/", "📡 Community Libraries" }
-                a { href: "https://github.com/DioxusLabs/sdk", "⚙️ Dioxus Development Kit" }
-                a { href: "https://marketplace.visualstudio.com/items?itemName=DioxusLabs.dioxus", "💫 VSCode Extension" }
-                a { href: "https://discord.gg/XgGxMSkvUM", "👋 Community Discord" }
+            
+            CardComponent {
+                position: Vec2::new(10., 10.),
+                width: 8.,
+                card,
+                skin,
             }
         }
     }
